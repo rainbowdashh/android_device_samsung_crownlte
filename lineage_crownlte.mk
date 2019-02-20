9e02f99e1404497cf82b0924f0eac3dec3afa2cd
Alex <razielrazi3@gmail.com>
2019-02-20 22:43:07 +0200

    lineageos bringup
    Change-Id: I8ceb0c6a5d9d8693ed9cb07d91aa4eb84654e5a1

--- lineage_crownlte.mk
+++ lineage_crownlte.mk
@@ -4,9 +4,9 @@
 $(call inherit-product, device/samsung/crownlte/full_crownlte.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_crownlte
+PRODUCT_NAME := lineage_crownlte
 
 BUILD_FINGERPRINT := samsung/star2ltexx/star2lte:8.0.0/R16NW/G965FXXU1ARCC:user/release-keys
 
